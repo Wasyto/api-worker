c8170dc75e48977326c66f6f5b137cd397e54704
leonardo-amaral <leozero103@gmail.com>
2023-03-22 19:13:33 -0300

    feat: filter method and responseTypes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { ApiWorkerMethod, ApiWorkerProps, ApiWorkerResponse } from './types';
 
-console.log('Test');
+console.log('Lost is gay!');
 
-function apiWorker({
+async function apiWorker({
   url,
   abortController,
   body,
@@ -13,6 +13,7 @@ function apiWorker({
   onError,
   onSuccess,
 }: ApiWorkerProps) {
+  console.log(typeof headers);
   try {
     if (typeof url !== 'string') throw 'url must be a string';
 
@@ -26,6 +27,61 @@ function apiWorker({
     }
 
     const isExternal = url.startsWith('http');
+
+    let apiWorkerConfig: any = {
+      method,
+      headers,
+    };
+
+    if (method !== ApiWorkerMethod.GET) {
+      if (typeof body == 'object') {
+        apiWorkerConfig = {
+          ...apiWorkerConfig,
+          body: JSON.stringify(body),
+        };
+      } else {
+        apiWorkerConfig = {
+          ...apiWorkerConfig,
+          body,
+        };
+      }
+    }
+
+    if (!isExternal) {
+      url = process.env.GATEWAY_API + url;
+    }
+    const request = await fetch(url, apiWorkerConfig);
+    if (!request.ok) {
+      throw request.status + ' - ' + request.statusText;
+    }
+    let response;
+
+    // I tried do a bomb, if you want cool, set it on fire!
+
+    // const formatedResponseType = (Object.keys(ApiWorkerResponse) as (keyof typeof ApiWorkerResponse)[]).map(
+    //   (key, index) => {
+    //     console.log(key);
+    //     if (responseType === key) {
+
+    //     }
+    //   },
+    // );
+
+    if (responseType === ApiWorkerResponse.JSON) {
+      response = request.json();
+    }
+    if (responseType === ApiWorkerResponse.BLOB) {
+      response = request.blob();
+    }
+    if (responseType === ApiWorkerResponse.ARRAY_BUFFER) {
+      response = request.arrayBuffer();
+    }
+    if (responseType === ApiWorkerResponse.TEXT) {
+      response = request.text();
+    }
+    if (responseType == null || undefined) {
+      throw 'Response type is not defined ' + responseType;
+    }
   } catch (err) {
     onError?.(err);
   }
